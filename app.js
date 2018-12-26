2ef55965ec2f29e301b3ff0244976bc578022a30
김종인 <jonginout@naver.com>
2018-12-27 01:53:49 +0900

    init

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res, next) => {
-  res.send(`<h1>Hello jongin!</h1>`)
+  res.send(`<h1>Hello jongin!!!</h1>`)
 })
 
 app.listen(3000, () => {
